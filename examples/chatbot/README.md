891038274b687300b1c4dde89f306b13c0cab97b
zjh <1152937237@qq.com>
2023-03-05 12:37:52 +0800

    fix ui bug

--- examples/chatbot/README.md
+++ examples/chatbot/README.md
@@ -9,16 +9,15 @@ pip install ipywidgets translate
 
 
 ```shell
-from ppllama.examples.ui import SimpleChatbot
-bot =  SimpleChatbot(
-				 usrname="User",
-                 botname="Chatbot",
-                 lang="en",
-                 max_gen_len=64,
-                 temperature=0.8,
-                 top_p=0.95,
-                 prompt=None)
-bot.load_model(ckpt_dir="ckpt/7B/", tokenizer_path="ckpt/tokenizer.model")
-bot.display()
+from examples.chatbot.ui import SimpleChatbot
+bot =  SimpleChatbot(usrname="User",
+                    botname="Chatbot",
+                    lang="en",
+                    max_gen_len=64,
+                    temperature=0.8,
+                    top_p=0.95,
+                    prompt=None)
+bot.load_generator(ckpt_dir="ckpt/7B/", tokenizer_path="ckpt/tokenizer.model")
+bot.show()
 ```
 
--- examples/chatbot/ui.py
+++ examples/chatbot/ui.py
@@ -1,17 +1,16 @@
 import ipywidgets as widgets
 from IPython.display import display, HTML
 from translate import Translator
-from functools import partial
 from ppllama import load_model, setup_model_parallel
 
 
 class SimpleChatbot:
     '''
         Usage:
-        from ppllama.examples.ui import SimpleChatbot
+        from examples.chatbot.ui import SimpleChatbot
         bot =  SimpleChatbot()
-        bot.load_model(ckpt_dir="ckpt/7B/", tokenizer_path="ckpt/tokenizer.model")
-        bot.display()
+        bot.load_generator(ckpt_dir="ckpt/7B/", tokenizer_path="ckpt/tokenizer.model")
+        bot.show()
     '''
     def __init__(self,
                  usrname="User",
@@ -20,11 +19,10 @@ class SimpleChatbot:
                  max_gen_len=64,
                  temperature=0.8,
                  top_p=0.95,
-                 prompt=None):
+                 prompt=""):
         self.usrname = usrname
         self.botname = botname
         self.lang = lang
-
         # translator
         self.translator_fwd = Translator(from_lang=lang, to_lang="en") if lang != "en" else None
         self.translator_bwd = Translator(from_lang="en", to_lang=lang) if lang != "en" else None
@@ -35,11 +33,36 @@ class SimpleChatbot:
         self.temperature = temperature
         self.top_p = top_p
 
-        self.prompt = "You are asked to pretend to be a human being in response to the user's " \
-                      "input, which is as follows:" if prompt is not None else prompt
+        self.prompt = prompt
 
         # initialize ui
         self.init_ui()
+        # bind button click event to function
+        self.send_button.on_click(self.on_send_button_click)
+        self.clear_button.on_click(self.on_clear_button_click)
+
+    def on_send_button_click(self, button):
+        with self.chat_history:
+            # get user input
+            user_message = self.user_input.value.strip()
+
+            # display user message
+            display(HTML(
+                f'<p style="margin: 0 0 5px 0; font-weight: bold;">{self.usrname}:</p><p style="margin: 0 0 20px 0;">{user_message}</p>'))
+
+            # TODO: Implement chatbot logic to generate reply
+            chatbot_message = self.chat(self.preprocess(user_message))
+            chatbot_message = self.postprocess(chatbot_message)
+
+            # display chatbot message
+            display(HTML(
+                f'<p style="margin: 0 0 5px 0; font-weight: bold;">{self.botname}:</p><p style="margin: 0 0 20px 0;">{chatbot_message}</p>'))
+
+            # clear user input
+            self.user_input.value = ''
+
+    def on_clear_button_click(self,button):
+        self.chat_history.clear_output()
 
     def init_ui(self):
         self.layout = widgets.Layout(
@@ -65,38 +88,17 @@ class SimpleChatbot:
 
         # create send button
         self.send_button = widgets.Button(description='Send', layout=widgets.Layout(width='100px'))
-
-        # bind button click event to function
-        self.send_button.on_click(on_send_button_click)
+        self.clear_button = widgets.Button(description='Clear', layout=widgets.Layout(width='100px'))
 
         # create chat UI
-        self.chat_ui = widgets.VBox([chat_history, widgets.HBox([user_input, send_button])], layout=layout)
+        self.chat_ui = widgets.VBox([self.chat_history, widgets.HBox([self.user_input, self.send_button])], layout=self.layout)
 
-    def display(self):
+    def show(self):
         # display chat UI
         display(self.chat_ui)
 
-    def on_send_button_click(self, button):
-        with chat_history:
-            # get user input
-            user_message = user_input.value.strip()
 
-            # display user message
-            display(HTML(
-                f'<p style="margin: 0 0 5px 0; font-weight: bold;">{self.usrname}:</p><p style="margin: 0 0 20px 0;">{user_message}</p>'))
-
-            # TODO: Implement chatbot logic to generate reply
-            chatbot_message = self.chat(self.preprocess(user_message))
-            chatbot_message = self.postprocess(chatbot_message)
-
-            # display chatbot message
-            display(HTML(
-                f'<p style="margin: 0 0 5px 0; font-weight: bold;">{self.botname}:</p><p style="margin: 0 0 20px 0;">{chatbot_message}</p>'))
-
-            # clear user input
-            user_input.value = ''
-
-    def load_model(self, ckpt_dir="ckpt/7B/", tokenizer_path="ckpt/tokenizer.model"):
+    def load_generator(self, ckpt_dir="ckpt/7B/", tokenizer_path="ckpt/tokenizer.model"):
         local_rank, world_size = setup_model_parallel()
         model, generator = load_model(ckpt_dir=ckpt_dir,
                                       tokenizer_path=tokenizer_path,
@@ -106,7 +108,7 @@ class SimpleChatbot:
 
     def chat(self, user_input):
         user_input = self.prompt + user_input
-        bot_response = generator.generate([user_input],
+        bot_response = self.generator.generate([user_input],
                                           max_gen_len=self.max_gen_len,
                                           temperature=self.temperature,
                                           top_p=self.top_p)
@@ -120,6 +122,6 @@ class SimpleChatbot:
 
     def postprocess(self, tgt_text):
         if self.translator_bwd is not None:
-            tgt_text = self.translator_fwd.translate(tgt_text)
+            tgt_text = self.translator_bwd.translate(tgt_text)
         return tgt_text
 

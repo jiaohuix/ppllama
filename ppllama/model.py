e97e21564781434ad05799b7a3b0517e79eb7626
jiaohui <68314259+jiaohuix@users.noreply.github.com>
2023-04-10 11:23:21 +0800

    Update model.py

--- ppllama/model.py
+++ ppllama/model.py
@@ -36,7 +36,7 @@ class RMSNorm(paddle.nn.Layer):
     def __init__(self, dim: int, eps: float = 1e-6):
         super().__init__()
         self.eps = eps
-        self.weight = paddle.create_parameter(shape=[dim],dtype="float32",default_initializer=I.Constant(0.))
+        self.weight = paddle.create_parameter(shape=[dim],dtype="float32",default_initializer=I.Constant(1.))
 
     def _norm(self, x):
         return x * paddle.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
